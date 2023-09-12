8fa43184793f4e39042f4d5081b0d8f91fefc321
Karnisha Karunakaran <karnisha.k@itp.objectfrontier.com>
2023-09-12 14:39:18 +0530

    sub

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more informat
+using System.Security;
+
+int[] n={20,40,30,50,60};
+int sub=n[0];
+for(int i=0;i<n.Length;i++){
+    sub=sub-n[i];
+
+}
+Console.WriteLine(sub);
