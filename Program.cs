c953bc803fb031553e8cb67ba24409b2ab2780d8
Priyadharshini Murugan <priyadharshini.mu@itp.objectfrontier.com>
2023-09-12 14:33:07 +0530

    first release

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+int [] a=new int[5]{2,5,8,5,6};
+int sum=0;
+for(int i=0;i<a.Length;i++)
+{
+    sum=sum+a[i];
+}
+{
+    Console.WriteLine(sum);
+}
